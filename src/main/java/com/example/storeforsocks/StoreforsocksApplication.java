d44b5f0c4cf9820535879773824c6f48c956f3f8
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-02-08 00:46:14 +0300

    первый коммит

---
+++ src/main/java/com/example/storeforsocks/StoreforsocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeforsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreforsocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreforsocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storeforsocks/StoreforsocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeforsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreforsocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
